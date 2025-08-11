495f013f72fb9b8adcd147e9e305840ed8566cc6
alexeymartinez-encore <amartinez@encoreautomation.com>
2025-08-11 13:12:38 -0400

    costcode change v3 console test v2

--- src/components/PortalComponents/AdminPage/ManageSheets/ManageTimesheets/ManageTimesheetsTable.jsx
+++ src/components/PortalComponents/AdminPage/ManageSheets/ManageTimesheets/ManageTimesheetsTable.jsx
@@ -144,7 +144,7 @@ export default function ManageTimesheetsTable({ onViewOvertime }) {
       entries.forEach((entry) => {
         // Map of day keys to simplify the iteration
         // let x = 0;
-        console.log(x, "=>", entry.entry);
+        console.log(entry);
         // x = x + 1;
         const dayKeys = [
           { day: "mon", index: 0 },
